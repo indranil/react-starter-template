8824fd9d8801646f1e46eea634f153eaec126084
Indranil Dasgupta <indranild@gmail.com>
2022-06-05 09:41:53 +0100

    add react

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl text-red-700">React Project</h1>
+    </div>
+  );
+}
